00d5c8dcf1fbca6ba0870a26f58cf030395ae7f0
ImmrAD <ananddane1@gmail.com>
2025-04-26 06:39:24 +0530

    1.10

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: true
-  }
+  },
+  connectionTimeoutMillis: 5000,
+  idleTimeoutMillis: 30000,
+  max: 20
 });
 
 pool.connect()
