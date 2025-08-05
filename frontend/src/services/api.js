3bf1a9b6d6b369eafa9b2194ecdf83d2ea385fb7
ImmrAD <ananddane1@gmail.com>
2025-08-05 21:23:07 +0530

    2.2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://the-digital-diner-4gd4.onrender.com/api', // add /api
+  baseURL: import.meta.env.VITE_API_URL || 'https://the-digital-diner-4gd4.onrender.com/api',
   withCredentials: true
 });
 
